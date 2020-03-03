6990fed1c4e33d87337e754fe85e227fdae20de3
VitorSamuelTI <vitorsamuel@msn.com>
2020-03-03 17:33:56 -0300

    XML Iteration Example with FOR

--- InterBroker/process.cpp
+++ InterBroker/process.cpp
@@ -19,7 +19,7 @@ int xmlProcess(char* message)
 {
 	string str(message);
 
-	printf(message);
+	//printf(message);
 
 	vector<char> xml_copy(str.begin(), str.end());
 	xml_copy.push_back('\0');
@@ -27,17 +27,25 @@ int xmlProcess(char* message)
 	xml_document<> doc;
 	doc.parse<parse_declaration_node | parse_no_data_nodes>(&xml_copy[0]);
 
-	cout << "Name of my first node is: " << doc.first_node()->name() << "\n";
+	//Get first Node example
+	//cout << "Name of my first node is: " << doc.first_node()->name() << "\n";
 
-	string encoding = doc.first_node()->first_attribute("encoding")->value();
-	cout << "encoding: " << encoding << endl;
+	//Get first attribute example
+	//string firstAttibure = doc.first_node()->first_attribute()->value();
+	//cout << "Att1: " << firstAttibure << endl;
 
-	//rapidxml::xml_node<>* node = doc.allocate_node(*/
 
-	/*for (xml_node<> *child = node->first_node(); child; child = child->next_sibling())
-	{
-			// do stuff with child
-	}*/
+	//Iteration Nodes Example
+	rapidxml::xml_node<>* node = doc.first_node();
+
+	for(node = node -> first_node(); node != NULL; node = node -> next_sibling()){
+
+      if(node->type() == node_element) //chek node type
+      {
+        cout<<node -> name() <<" 's value is : "<<node->value() <<   endl; //get node value
+        
+      }
+    }
 
 	return 0;
 }
